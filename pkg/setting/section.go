e66772c9f0b8cccf4a57b3d51578a77208c0791c
kliu48 <kliu48@hawk.iit.edu>
2022-06-26 11:09:45 +0800

    server config with yaml read

---
+++ pkg/setting/section.go
@@ -0,0 +1,40 @@
+package settting
+
+import "time"
+
+type ServerSettings struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type AppSettingS struct {
+	DefaultPageSize int
+	MaxPageSize     int
+	LogSavePath     string
+	LogFileName     string
+	LogFileExt      string
+}
+
+type DatabaseSettings struct {
+	DBType       string
+	Username     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    string
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+// ReadSection 读取yaml的配置信息/
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
