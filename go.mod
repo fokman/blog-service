40a29018da3d3bb126c57d590c282dae160d8411
kliu48 <kliu48@hawk.iit.edu>
2022-06-30 20:38:09 +0800

    add common code

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"fmt"
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"github.com/jinzhu/gorm"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreateBy   string `json:"create_by"`
@@ -8,4 +15,25 @@ type Model struct {
 	ModifiedOn string `json:"modified_on"`
 	DeleteOn   string `json:"delete_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+// NewDBEngine 数据库连接配置处理
+func NewDBEngine(databaseSetting *setting.DatabaseSettings) (*gorm.DB, error) {
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t@Local",
+		databaseSetting.Username,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime))
+	if err != nil {
+		return nil, err
+	}
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	return db, nil
 }
