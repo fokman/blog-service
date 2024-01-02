3673b2dcf04931b0eba0de34ae660a1f85c9bffc
kliu <liukun@cmsr.chinamobile.com>
2024-01-02 09:11:54 +0800

    blog template code

---
+++ internal/sql2struct/template.go
@@ -0,0 +1,49 @@
+package sql2struct
+
+import (
+	"fmt"
+)
+
+const strcutTpl = `type {{.TableName | ToCamelCase}} struct {
+{{range .Columns}}	{{ $length := len .Comment}} {{ if gt $length 0 }}// {{.Comment}} {{else}}// {{.Name}} {{ end }}
+	{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
+{{end}}}
+
+func (model {{.TableName | ToCamelCase}}) TableName() string {
+	return "{{.TableName}}"
+}`
+
+// StructTemplate 结构体模版
+type StructTemplate struct {
+	structTpl string
+}
+
+type StructColumn struct {
+	Name    string
+	Type    string
+	Tag     string
+	Comment string
+}
+
+type StructTableName struct {
+	TableName string
+	Columns   []*StructColumn
+}
+
+func NewStructTemplate() *StructTemplate {
+	return &StructTemplate{structTpl: strcutTpl}
+}
+
+func (t *StructTemplate) AssemblyColumns(tbColumns []*TableColumn) []*StructColumn {
+	tplColumns := make([]*StructColumn, 0, len(tbColumns))
+	for _, column := range tbColumns {
+		tag := fmt.Sprintf("`"+"json:"+"\"%s\""+"`", column.ColumnName)
+		tplColumns = append(tplColumns, &StructColumn{
+			Name:    column.ColumnName,
+			Type:    DBTypeToStructType[column.DataType],
+			Tag:     tag,
+			Comment: column.ColumnComment,
+		})
+	}
+	return tplColumns
+}
